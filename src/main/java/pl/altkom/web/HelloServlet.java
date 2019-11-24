2c76f90619ca5ed19b3cba7f58e859f1c2e27c73
Paweł Bogdan <pawel.bogdan1@gmail.com>
2019-11-24 12:12:10 +0100

    Initial commit

---
+++ src/main/java/pl/altkom/web/HelloServlet.java
@@ -0,0 +1,23 @@
+package pl.altkom.web;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    public void service(HttpServletRequest request,
+                        HttpServletResponse response)
+            throws IOException {
+        PrintWriter pw = response.getWriter();
+        pw.println("<HTML><HEAD>");
+        pw.println("<meta charset=\"UTF-8\">");
+        pw.println("<TITLE>Hello</TITLE>");
+        pw.println("</HEAD><BODY>");
+        pw.println("<H3>Hello Paweł!</H3>");
+        pw.println("</BODY></HTML>");
+    }
+}
