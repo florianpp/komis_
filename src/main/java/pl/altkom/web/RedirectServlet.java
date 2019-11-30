0366409cd8bfdcd6ddb3b589ef017611014a133e
Paweł Bogdan <pawel.bogdan1@gmail.com>
2019-11-30 10:51:09 +0100

    quick fix

--- src/main/java/pl/altkom/web/RedirectServlet.java
+++ src/main/java/pl/altkom/web/RedirectServlet.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/process")
 public class RedirectServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
